3a9d08c39bee4386e2510fc7350e565cc530e90f
sandro sviben <sandro.sviben@hotmail.com>
2017-02-23 11:45:06 +0100

    komentari sto sve treba napraviti

---
+++ PrimjerDictionary/PrimjerDictionary/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimjerDictionary
+{   //kreiraj klasu Customer, te joj dodijeli ID, Ime, Placa
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: Kreiraj Dictionary, CustomerID je kljuc, Tip je int
+            //Customer objekt je vrijednost, tip je Customer
+
+            //Kreiraj 3 Customer objekta te im inicijaliziraj vrijednosti ID, Ime, Placa
+
+            //Dodaj customer objekte u dictionary
+
+            //dohvati i ispisi vrijednost customer objekata iz dictionary-a koristeci kljuc (CustomerID). Najbrzi nacin dohvacanja vrijednosti iz dictionary-a je koristenjem kljuceva
+
+            //dohvati i ispisi vrijednost svih objekata u dictionary-u
+
+            //dohvati i ispisi vrijednost svih objekata u dictionary-u koristeci VAR
+
+            //dohvati i ispisi sve kljuceve u dictionary-u
+
+            //dohvati i ispisi sve vrijednosti u dictionary-u
+            
+            //dodaj kljuc koji vec postoji u dictionary. Sto zakljucujes?
+
+            //provjeri da li kljuc vec postoji u dictionary-u
+
+            //provjera da li postoji kljuc po kojemu pretrazujemo u dictionaryu. Ako ne postoji sto ce se desiti?
+        }
+    }
+}
